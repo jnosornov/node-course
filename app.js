add7bbc70f1cd21fe8a2e7db06a7ae96eb2a88be
Johnathan Osorno <jnosornov@gmail.com>
2024-10-08 14:03:23 -0500

    middlewear execution

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middlewear!");
-  next(); // allows the request to continue to next middlewear in line
+
+app.use('/', (req, res, next) => {
+  console.log("This always runs!");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log("In another middlewear!");
+  res.send('<h1>The "Add Product" Page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log("In another middlewear!");
   res.send('<h1>Hello from Express!</h1>');
 });
